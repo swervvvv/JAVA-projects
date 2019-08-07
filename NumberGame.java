3080f15202e4ba5217098291fc94025393ddd84a
swervvvv <mwilsonjr13@gmail.com>
2019-08-07 08:36:39 -0500

    adding files

---
+++ NumberGame.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+public class NumberGame{
+	public static void main(String[] args){
+		
+		//create 2 random numbers between 0-20
+		Random random = new Random();
+		Scanner scanner = new Scanner(System.in);
+		int counter = 0;
+		int number = random.nextInt(100);
+		
+		boolean answer = false;
+		while(answer != true)
+		{
+			System.out.println("Pick a number between 1 and 100: ");
+			int user = scanner.nextInt(); 
+			if (number == user) 
+			{
+				System.out.println("you got it right");	
+				answer = true;
+			}
+			else if (user > 100) 
+			{
+				System.out.println("you number is out of the range");
+			}
+			else 
+			{
+				answer = false;
+				System.out.println("wrong");
+
+			}
+		}
+		
+			
+		
+	}
+}
